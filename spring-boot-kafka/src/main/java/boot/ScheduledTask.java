18b160f260b1a38678e14da3a073a6bb824b1500
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-03-25 12:54:45 +0800

    add spring boot kafka
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ spring-boot-kafka/src/main/java/boot/ScheduledTask.java
@@ -0,0 +1,25 @@
+package boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTask {
+  private final Producer producer;
+  private int counter;
+
+  @Autowired
+  public ScheduledTask(Producer producer) {
+    this.producer = producer;
+  }
+
+  @Scheduled(fixedDelay = 5000)
+  public void task() {
+    producer.sendMessage("Message " + counter);
+    counter++;
+    if (counter == 5) {
+      System.exit(0);
+    }
+  }
+}
