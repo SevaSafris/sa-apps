f974eeb44c530b7258ef81eddc58c0f2eadbc6ab
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-03-26 15:27:51 +0800

    add jdbc
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- jdbc/src/main/java/jdbc/App.java
+++ jdbc/src/main/java/jdbc/App.java
@@ -3,7 +3,6 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class App {
