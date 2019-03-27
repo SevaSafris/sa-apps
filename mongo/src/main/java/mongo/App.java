2f41665ec487490559c42f54d0b5ba491c3c2f17
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-03-27 13:47:59 +0800

    add mongo
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ mongo/src/main/java/mongo/App.java
@@ -0,0 +1,44 @@
+package mongo;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.concurrent.TimeUnit;
+import org.bson.Document;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    final com.mongodb.client.MongoClient client = createClient();
+
+    // This one doesn't work:
+    // MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+    MongoDatabase database = client.getDatabase("myMongoDb");
+    if (database.getCollection("customers") == null) {
+      database.createCollection("customers");
+    }
+    MongoCollection<Document> collection = database.getCollection("customers");
+    Document document = new Document();
+    document.put("name", "Name");
+    document.put("company", "Company");
+    collection.insertOne(document);
+    collection.find().first();
+
+    client.close();
+
+    TimeUnit.SECONDS.sleep(30);
+  }
+
+  private static com.mongodb.client.MongoClient createClient() {
+    final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+        .applyConnectionString(new ConnectionString("mongodb://localhost:27017")).build();
+
+    return MongoClients.create(mongoClientSettings);
+
+    // This one doesn't work:
+    //return MongoClients.create("mongodb://localhost:27017");
+
+  }
+}
