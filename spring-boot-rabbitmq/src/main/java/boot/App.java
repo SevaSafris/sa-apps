5daaaaeaf60f3c37de7d3b6de25d03570248f64a
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-04-15 21:42:45 +0800

    add spring boot rabbitmq app
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ spring-boot-rabbitmq/src/main/java/boot/App.java
@@ -0,0 +1,85 @@
+package boot;
+
+import com.rabbitmq.client.Channel;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+import java.util.Map.Entry;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+  static final String topicExchangeName = "spring-boot-exchange";
+
+  static final String queueName = "spring-boot";
+  static final String queueName2 = "spring-boot-2";
+
+  @Bean
+  Queue queue() {
+    return new Queue(queueName, false);
+  }
+
+  @Bean
+  public Queue queue2() {
+    return new Queue(queueName2);
+  }
+
+  @Bean
+  TopicExchange exchange() {
+    return new TopicExchange(topicExchangeName);
+  }
+
+  @Bean
+  Binding binding(Queue queue, TopicExchange exchange) {
+    return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+  }
+
+  @Bean
+  SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+      MessageListenerAdapter listenerAdapter) {
+    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.setQueueNames(queueName);
+    container.setMessageListener(listenerAdapter);
+    return container;
+  }
+
+  @Bean
+  MessageListenerAdapter listenerAdapter(Receiver receiver) {
+    return new MessageListenerAdapter(receiver, "receiveMessage") {
+      @Override
+      public void onMessage(Message message, Channel channel) throws Exception {
+        for (Entry<String, Object> entry : message.getMessageProperties().getHeaders()
+            .entrySet()) {
+          System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+        final Tracer tracer = GlobalTracer.get();
+        System.out.println(tracer.scopeManager().active());
+        System.out.println("onMessage START");
+        super.onMessage(message, channel);
+        System.out.println("onMessage End");
+      }
+    };
+  }
+
+  @RabbitListener(queues = queueName2)
+  public void listen(String message) {
+    System.out.println("Received <" + message + ">");
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args).close();
+  }
+
+}
+
---
+++ spring-boot-rabbitmq/src/main/java/boot/Runner.java
@@ -0,0 +1,30 @@
+package boot;
+
+import java.util.concurrent.TimeUnit;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+  private final RabbitTemplate rabbitTemplate;
+  private final Receiver receiver;
+
+  public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+    this.receiver = receiver;
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Sending message to Queue 1...");
+    rabbitTemplate.convertAndSend(App.topicExchangeName, "foo.bar.baz", "Message for Queue 1");
+
+    System.out.println("Sending message to Queue 2...");
+    rabbitTemplate.convertAndSend(App.queueName2, "Message for Queue 2");
+
+    TimeUnit.SECONDS.sleep(30);
+  }
+
+}
