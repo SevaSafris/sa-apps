94c556af168e0196402a659090812c44952d6c69
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-04-15 20:33:41 +0800

    add Jedis-2 app
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ jedis-2/src/main/java/jedis2/App.java
@@ -0,0 +1,14 @@
+package jedis2;
+
+import java.util.concurrent.TimeUnit;
+import redis.clients.jedis.Jedis;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    Jedis jedis = new Jedis();
+    jedis.set("key", "value");
+    System.out.println(jedis.get("key"));
+
+    TimeUnit.SECONDS.sleep(30L);
+  }
+}
