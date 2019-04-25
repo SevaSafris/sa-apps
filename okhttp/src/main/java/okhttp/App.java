9974d3d5817f8bd1a6861ca331e68b7626c5ebd4
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-04-25 20:32:45 +0800

    use okhttp constructor and builder
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- okhttp/src/main/java/okhttp/App.java
+++ okhttp/src/main/java/okhttp/App.java
@@ -1,25 +1,24 @@
 package okhttp;
 
+import java.io.IOException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class App {
   public static void main(String[] args) throws Exception {
+    test(new OkHttpClient.Builder().build());
+    test(new OkHttpClient());
 
-    final OkHttpClient client = new OkHttpClient.Builder().build();
+    Thread.sleep(30_000);
+  }
 
-    // This one doesn't work:
-    // OkHttpClient client = new OkHttpClient();
-
+  private static void test(OkHttpClient client) throws IOException {
     Request request = new Request.Builder()
         .url("https://www.google.com")
         .build();
 
     Response response = client.newCall(request).execute();
-
     System.out.println(response.code());
-
-    Thread.sleep(30_000);
   }
 }
