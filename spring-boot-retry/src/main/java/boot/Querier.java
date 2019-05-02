2cd251d359e0df85b37bb61e99a356fd92fbe3d8
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-05-02 10:39:54 +0800

    update
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- spring-boot-retry/src/main/java/boot/Querier.java
+++ spring-boot-retry/src/main/java/boot/Querier.java
@@ -3,7 +3,6 @@ package boot;
 import java.io.IOException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
@@ -16,6 +15,7 @@ public class Querier {
 
   @Retryable(interceptor = "retryInterceptor")
   public void get() throws IOException {
+    System.out.println("Execute");
     httpClient.execute(new HttpGet("localhost:9999"));
   }
 
