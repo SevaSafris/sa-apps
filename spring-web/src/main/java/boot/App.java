dbcf4b2a108719063e242b8b60199821856de2a9
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-05-02 10:32:53 +0800

    update
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- spring-web/src/main/java/boot/App.java
+++ spring-web/src/main/java/boot/App.java
@@ -1,43 +1,20 @@
 package boot;
 
 import java.util.concurrent.TimeUnit;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 public class App {
 
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+  public static void main(String[] args) throws Exception {
+    RestTemplate restTemplate = new RestTemplate();
+    final ResponseEntity<String> entity = restTemplate
+        .getForEntity("http://www.google.com", String.class);
 
-  @Autowired
-  private RestTemplate restTemplate;
+    System.out.println(entity.getStatusCode());
 
-  public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
-  }
-
-  @Bean
-  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-    return args -> {
-
-      final ResponseEntity<String> entity = restTemplate
-          .getForEntity("http://localhost:8080", String.class);
-
-      System.out.println(entity);
-
-      TimeUnit.SECONDS.sleep(30);
-      System.exit(0);
-
-    };
+    TimeUnit.SECONDS.sleep(30);
   }
 
 }
+
--- spring-webmvc/src/main/java/boot/HelloController.java
+++ spring-webmvc/src/main/java/boot/HelloController.java
@@ -1,5 +1,7 @@
 package boot;
 
+import io.opentracing.Span;
+import io.opentracing.util.GlobalTracer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,8 @@ public class HelloController {
 
   @RequestMapping("/")
   public String index() {
+    final Span span = GlobalTracer.get().activeSpan();
+    System.out.println("Active span: " + span);
     return "Greetings from Spring Boot!";
   }
 
