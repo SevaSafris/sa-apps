02fcc22ba99eea4f7e9790871e80df8e9fa37c13
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-05-07 20:45:51 +0800

    add redisson app
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ redisson/src/main/java/redisson/App.java
@@ -0,0 +1,25 @@
+package redisson;
+
+import java.util.concurrent.TimeUnit;
+import org.redisson.Redisson;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+public class App {
+  public static void main(String[] args) throws InterruptedException {
+    Config config = new Config();
+    config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+
+    RedissonClient redissonClient = Redisson.create(config);
+
+    RMap<String, String> map = redissonClient.getMap("map");
+
+    map.put("key", "value");
+    System.out.println("value=" + map.get("key"));
+
+    redissonClient.shutdown();
+
+    TimeUnit.SECONDS.sleep(30);
+  }
+}
