32e15697a14f85c2942246ccd81e651a5c191604
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-05-28 22:13:26 +0800

    update spring kafka app
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- spring-boot-kafka/src/main/java/boot/Consumer.java
+++ spring-boot-kafka/src/main/java/boot/Consumer.java
@@ -1,18 +1,22 @@
 package boot;
 
 import java.io.IOException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
 
-  private final Logger logger = LoggerFactory.getLogger(Producer.class);
-
   @KafkaListener(topics = "users", groupId = "group_id")
-  public void consume(String message) throws IOException {
-    System.out.println(String.format("#### -> Consumed message -> %s", message));
+  @SendTo("reply")
+  public String consume(String message) throws IOException {
+    System.out.println(String.format("1. #### -> Consumed message -> %s", message));
+    return message.toUpperCase();
+  }
+
+  @KafkaListener(topics = "reply", groupId = "group_id")
+  public void consume2(String message) throws IOException {
+    System.out.println(String.format("2. #### -> Consumed message -> %s", message));
   }
 }
