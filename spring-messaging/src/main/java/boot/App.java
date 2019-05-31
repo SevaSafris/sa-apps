d719d11d21cd75e0e200b522ea51a4ea3f543051
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-05-31 16:06:32 +0800

    add spring messaging (stream) app
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ spring-messaging/src/main/java/boot/App.java
@@ -0,0 +1,39 @@
+package boot;
+
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+  @Autowired
+  private Sender sender;
+
+  @Autowired
+  private Receiver receiver;
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+      sender.send("Ping");
+      int counter = 0;
+      while (receiver.getReceivedMessages().size() == 0 && counter < 10) {
+        TimeUnit.SECONDS.sleep(1);
+        counter++;
+      }
+
+      System.out.println("Received: " + receiver.getReceivedMessages());
+      TimeUnit.SECONDS.sleep(10);
+      System.exit(0);
+
+    };
+  }
+}
---
+++ spring-messaging/src/main/java/boot/Receiver.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2017-2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package boot;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+/**
+ * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
+ */
+@EnableBinding(Sink.class)
+public class Receiver {
+
+  private final List<String> receivedMessages = new ArrayList<>();
+
+  @StreamListener(Sink.INPUT)
+  public void receive(String message) {
+    receivedMessages.add(message);
+  }
+
+  public List<String> getReceivedMessages() {
+    return receivedMessages;
+  }
+
+  public void clear() {
+    receivedMessages.clear();
+  }
+
+}
