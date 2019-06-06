65b1ce5750a4f4b8a0107d1548af4afc168bea9e
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-06-06 21:08:13 +0800

    add echo
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 cd spring-web
+echo "##############################################################################################"
 echo "spring-web"
 make build run-travis-test || exit $?
 cd ..
 
 cd spring-scheduling
+echo "##############################################################################################"
 echo "spring-scheduling"
 make build run-travis-test || exit $?
 cd ..
