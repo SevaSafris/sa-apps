fcd23c7ac4df9f4c9ef5496c4496aabf2a45880b
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-06-14 12:42:34 +0800

    add spring-boot-es
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- test.sh
+++ test.sh
@@ -93,6 +93,12 @@ docker pull docker.elastic.co/elasticsearch/elasticsearch:6.6.1
 docker run --name es6 -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.6.1
 sleep 10
 make build run-travis-test || exit $?
+cd ..
+
+cd spring-boot-elasticsearch
+echo "##############################################################################################"
+echo "spring-boot-elasticsearch"
+make build run-travis-test || exit $?
 docker stop es6
 cd ..
 
