2ae7cf61c3e74d6cb34ad6abde4d6c0464d21e19
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-07-09 22:04:35 +0800

    update httpclient
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- httpclient/src/main/java/httpclient/App.java
+++ httpclient/src/main/java/httpclient/App.java
@@ -1,18 +1,17 @@
 package httpclient;
 
-import java.util.concurrent.TimeUnit;
-import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
+import util.Util;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    final CloseableHttpClient client = HttpClients.createDefault();
-    final CloseableHttpResponse response = client.execute(new HttpGet("http://www.google.com"));
+    final HttpClient client = HttpClientBuilder.create().build();
+    final HttpResponse response = client.execute(new HttpGet("http://www.google.com"));
     System.out.println(response.getStatusLine().getReasonPhrase());
-    client.close();
 
-    TimeUnit.SECONDS.sleep(10);
+    Util.checkSpan("java-httpclient", 1);
   }
 }
---
+++ httpclient/src/main/java/util/Util.java
@@ -0,0 +1,44 @@
+package util;
+
+import io.opentracing.mock.MockSpan;
+import io.opentracing.mock.MockTracer;
+import io.opentracing.tag.Tags;
+import io.opentracing.util.GlobalTracer;
+import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
+
+public class Util {
+
+  public static void checkSpan(String component, int spanCount) throws Exception {
+    final Field field = GlobalTracer.get().getClass().getDeclaredField("tracer");
+    field.setAccessible(true);
+    final Object ob = field.get(GlobalTracer.get());
+    MockTracer tracer;
+    if (ob instanceof MockTracer) {
+      tracer = (MockTracer) ob;
+    } else {
+      TimeUnit.SECONDS.sleep(10);
+      return;
+    }
+    boolean found = false;
+    System.out.println("Spans: " + tracer.finishedSpans());
+    for (MockSpan span : tracer.finishedSpans()) {
+      System.out.println(span.tags().get(Tags.COMPONENT.getKey()));
+      if (span.tags().get(Tags.COMPONENT.getKey()).equals(component)) {
+        found = true;
+        System.out.println("Found " + component + " span");
+        break;
+      }
+    }
+    if (!found) {
+      System.err.println(component + " span not found");
+      System.exit(-1);
+    }
+
+    if (tracer.finishedSpans().size() != spanCount) {
+      throw new RuntimeException(tracer.finishedSpans().size() +
+          " spans instead of " + spanCount);
+    }
+  }
+
+}
