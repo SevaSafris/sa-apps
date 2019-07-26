0c6556803458657d124170df3fd0072b5510cf40
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-07-26 21:27:36 +0800

    update websocket app
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- spring-websocket/src/main/java/boot/HelloController.java
+++ spring-websocket/src/main/java/boot/HelloController.java
@@ -1,5 +1,6 @@
 package boot;
 
+import io.opentracing.util.GlobalTracer;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class HelloController {
   @SendTo("/topic/greetings")
   public String greeting(String message) {
     System.out.println("Greeting: " + message);
+    if (GlobalTracer.get().activeSpan() == null) {
+      System.err.println("No active span");
+      System.exit(-1);
+    }
+    System.out.println("Active span: " + GlobalTracer.get().activeSpan());
     return message.toUpperCase();
   }
 
