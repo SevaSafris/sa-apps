4007eb0dae791333075d7f9dc38e8ff85c353f0d
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-07-30 21:21:12 +0800

    start/stop es6/es7
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- test.sh
+++ test.sh
@@ -116,16 +116,20 @@ cd ..
 cd elasticsearch-6
 echo "##############################################################################################"
 echo "elasticsearch-6"
-docker pull docker.elastic.co/elasticsearch/elasticsearch:6.6.1
-docker run --name es6 -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.6.1
+docker pull docker.elastic.co/elasticsearch/elasticsearch:6.8.1
+docker run --name es6 -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.8.1
 sleep 10
 make build run-mocktracer-test || exit $?
+docker stop es6
 cd ..
 
 cd elasticsearch-7
 echo "##############################################################################################"
 echo "elasticsearch-7"
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.2.0
+docker run --name es7 -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.2.0
 make build run-mocktracer-test || exit $?
+docker stop es7
 cd ..
 
 cd cassandra
