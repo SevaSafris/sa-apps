81a67ae0a2b40ac4e77556abeafe6fede5de3c13
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-08-21 15:58:51 +0800

    use embedded cassandra
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- cassandra/src/main/java/cassandra/App.java
+++ cassandra/src/main/java/cassandra/App.java
@@ -3,18 +3,21 @@ package cassandra;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
 import util.Util;
 
 public class App {
   public static void main(String[] args) throws Exception {
+    EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+    EmbeddedCassandraServerHelper.getSession();
 
-    Cluster cluster = Cluster.builder().addContactPoints("127.0.0.1").withPort(9042).build();
-    Session session;
+    Cluster cluster = Cluster.builder().addContactPoints("127.0.0.1").withPort(9142).build();
+    Session session = null;
     try {
       session = cluster.connect();
     } catch (Exception e) {
       System.err.println("Cannot connect, exiting.\n" + e.getMessage());
-      return;
+      System.exit(-1);
     }
 
     ResultSet resultSet = session.execute("select * from system.compaction_history");
@@ -23,7 +26,11 @@ public class App {
     session.close();
     cluster.close();
 
+    EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
+
     Util.checkSpan("java-cassandra", 1);
+
+    System.exit(0);
   }
 
 
