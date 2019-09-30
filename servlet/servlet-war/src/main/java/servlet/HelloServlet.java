f614c519a884f5c65df6fc284bfdeb563197492d
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-09-30 21:16:49 +0800

    add tracer
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- servlet/servlet-war/src/main/java/servlet/HelloServlet.java
+++ servlet/servlet-war/src/main/java/servlet/HelloServlet.java
@@ -3,6 +3,7 @@ package servlet;
 import io.opentracing.util.GlobalTracer;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,15 @@ public class HelloServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     PrintWriter out = resp.getWriter();
+    try {
+      final Field field = GlobalTracer.get().getClass().getDeclaredField("tracer");
+      field.setAccessible(true);
+      final Object tracer = field.get(GlobalTracer.get());
+      out.println("Tracer: " + tracer);
+
+    } catch (Exception ignore) {
+    }
+
     out.println("Active span: " + GlobalTracer.get().activeSpan());
 
     out.close();
